eed80fdd3987ff92032e1efda808b6d8207ee0e3
dragon <gildydtjd@naver.com>
2022-01-19 13:38:12 +0900

    Feat : 01.19 강의 정리 및 프로젝트

---
+++ 0119/0119/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace _0119
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.DContain = new System.Windows.Forms.Label();
+            this.DEquals = new System.Windows.Forms.Label();
+            this.DLength = new System.Windows.Forms.Label();
+            this.DReplace = new System.Windows.Forms.Label();
+            this.DSplit = new System.Windows.Forms.Label();
+            this.DSubstring = new System.Windows.Forms.Label();
+            this.DToLower = new System.Windows.Forms.Label();
+            this.DToUpper = new System.Windows.Forms.Label();
+            this.DTrim = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(238, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WinForm_StringTest";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Equals";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Length";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Replace";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(137, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Split";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(457, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Substring";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(457, 190);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "ToLower";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(457, 240);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "ToUpper";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(457, 285);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Trim";
+            // 
+            // Contain
+            // 
+            this.DContain.AutoSize = true;
+            this.DContain.Location = new System.Drawing.Point(200, 133);
+            this.DContain.Name = "Contain";
+            this.DContain.Size = new System.Drawing.Size(15, 15);
+            this.DContain.TabIndex = 11;
+            this.DContain.Text = "-";
+            // 
+            // Equals
+            // 
+            this.DEquals.AutoSize = true;
+            this.DEquals.Location = new System.Drawing.Point(200, 190);
+            this.DEquals.Name = "Equals";
+            this.DEquals.Size = new System.Drawing.Size(15, 15);
+            this.DEquals.TabIndex = 12;
+            this.DEquals.Text = "-";
+            // 
+            // Length
+            // 
+            this.DLength.AutoSize = true;
+            this.DLength.Location = new System.Drawing.Point(200, 240);
+            this.DLength.Name = "Length";
+            this.DLength.Size = new System.Drawing.Size(15, 15);
+            this.DLength.TabIndex = 13;
+            this.DLength.Text = "-";
+            // 
+            // Replace
+            // 
+            this.DReplace.AutoSize = true;
+            this.DReplace.Location = new System.Drawing.Point(200, 285);
+            this.DReplace.Name = "Replace";
+            this.DReplace.Size = new System.Drawing.Size(15, 15);
+            this.DReplace.TabIndex = 14;
+            this.DReplace.Text = "-";
+            // 
+            // Split
+            // 
+            this.DSplit.AutoSize = true;
+            this.DSplit.Location = new System.Drawing.Point(200, 329);
+            this.DSplit.Name = "Split";
+            this.DSplit.Size = new System.Drawing.Size(15, 15);
+            this.DSplit.TabIndex = 15;
+            this.DSplit.Text = "-";
+            // 
+            // Substring
+            // 
+            this.DSubstring.AutoSize = true;
+            this.DSubstring.Location = new System.Drawing.Point(531, 133);
+            this.DSubstring.Name = "Substring";
+            this.DSubstring.Size = new System.Drawing.Size(15, 15);
+            this.DSubstring.TabIndex = 16;
+            this.DSubstring.Text = "-";
+            // 
+            // ToLower
+            // 
+            this.DToLower.AutoSize = true;
+            this.DToLower.Location = new System.Drawing.Point(531, 190);
+            this.DToLower.Name = "ToLower";
+            this.DToLower.Size = new System.Drawing.Size(15, 15);
+            this.DToLower.TabIndex = 17;
+            this.DToLower.Text = "-";
+            // 
+            // ToUpper
+            // 
+            this.DToUpper.AutoSize = true;
+            this.DToUpper.Location = new System.Drawing.Point(531, 240);
+            this.DToUpper.Name = "ToUpper";
+            this.DToUpper.Size = new System.Drawing.Size(15, 15);
+            this.DToUpper.TabIndex = 18;
+            this.DToUpper.Text = "-";
+            // 
+            // Trim
+            // 
+            this.DTrim.AutoSize = true;
+            this.DTrim.Location = new System.Drawing.Point(531, 285);
+            this.DTrim.Name = "Trim";
+            this.DTrim.Size = new System.Drawing.Size(15, 15);
+            this.DTrim.TabIndex = 19;
+            this.DTrim.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕 Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(578, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 32);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DTrim);
+            this.Controls.Add(this.DToUpper);
+            this.Controls.Add(this.DToLower);
+            this.Controls.Add(this.DSubstring);
+            this.Controls.Add(this.DSplit);
+            this.Controls.Add(this.DReplace);
+            this.Controls.Add(this.DLength);
+            this.Controls.Add(this.DEquals);
+            this.Controls.Add(this.DContain);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label DContain;
+        private System.Windows.Forms.Label DEquals;
+        private System.Windows.Forms.Label DLength;
+        private System.Windows.Forms.Label DReplace;
+        private System.Windows.Forms.Label DSplit;
+        private System.Windows.Forms.Label DSubstring;
+        private System.Windows.Forms.Label DToLower;
+        private System.Windows.Forms.Label DToUpper;
+        private System.Windows.Forms.Label DTrim;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 0119/0119/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0119
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        public void StringHooks(string f, string data, string about = default, string about2 = default, char about3 = default, int about4 = default, int about5 = default)
+        {
+            if (f!=null) {
+                switch (f)
+                {
+                    case "Contain" : DContain.Text = data.Contains(about).ToString(); break;
+                    case "Equals" : DEquals.Text = data.Equals(about).ToString(); break;
+                    case "Length" : DLength.Text = data.Length.ToString(); break;
+                    case "Replace" : DReplace.Text = data.Replace(about, about2).ToString(); break;
+                    case "Split" : { string[] strSplit = data.Split(about3);  DSplit.Text = strSplit[0].ToString(); } break;
+                    case "Substring" : DSubstring.Text = data.Substring(about4, about5).ToString(); break;
+                    case "ToUpper" : DToUpper.Text = data.ToUpper().ToString(); break;
+                    case "ToLower" : DToLower.Text = data.ToLower().ToString(); break;
+                    case "Trim" : DTrim.Text = data.Trim().ToString(); break;
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strText = label1.Text;
+
+            DContain.Text = strText.Contains("Test").ToString();
+            DEquals.Text = strText.Equals("Test").ToString();
+            DLength.Text = strText.Length.ToString();
+            DReplace.Text = strText.Replace("Test", "draon").ToString();
+
+            string[] strSplit = strText.Split('_');
+            DSplit.Text = strSplit[0].ToString();
+
+            DSubstring.Text = strText.Substring(3, 5).ToString();
+
+            DToLower.Text = strText.ToLower().ToString();
+            DToUpper.Text = strText.ToUpper().ToString();
+
+            DTrim.Text = strText.Trim().ToString();
+        }
+    }
+}
