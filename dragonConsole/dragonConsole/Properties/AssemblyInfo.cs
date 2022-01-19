f173e804723dc96a39752ed319415e839a0f5d7c
dragon <gildydtjd@naver.com>
2022-01-19 12:07:39 +0900

    Feat: 01.19 강의(string, DataType, method, Operator, Enum)

--- dragonConsole/dragonConsole/Properties/AssemblyInfo.cs
+++ dragonConsole/dragonConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Console")]
+[assembly: AssemblyTitle("dragonConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Console")]
+[assembly: AssemblyProduct("dragonConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("6e29f21f-7f6f-4bbd-bfc3-6ac9e003d518")]
+[assembly: Guid("0e05b119-83ea-4ab3-a978-1463d116ef05")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
