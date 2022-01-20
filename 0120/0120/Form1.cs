dca8f2b2ccb698a94d4bfff59ce5ca333c65ecc5
dragon <gildydtjd@naver.com>
2022-01-20 10:30:11 +0900

    REFACTOR : codeReview 후 문제점 수정

--- 0120/0120/Form1.cs
+++ 0120/0120/Form1.cs
@@ -16,28 +16,12 @@ namespace _0120
         {
             InitializeComponent();
         }
+        int[,] ArrayTest = { { 1, 2, 3, 4, 5, 6, 7 }, { 8, 9, 10, 11, 12, 13, 14 } };
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] ArrayTest = { 1, 2, 3, 4, 5, 6, 7 };
-
             dgDay.Rows.Clear();
 
-            dgDay["colDay1", 0].Value = ArrayTest[0];
-            dgDay["colDay2", 0].Value = ArrayTest[1];
-            dgDay["colDay3", 0].Value = ArrayTest[2];
-            dgDay["colDay4", 0].Value = ArrayTest[3];
-            dgDay["colDay5", 0].Value = ArrayTest[4];
-            dgDay["colDay6", 0].Value = ArrayTest[5];
-            dgDay["colDay7", 0].Value = ArrayTest[6];
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int[,] ArrayTest = { { 1, 2, 3, 4, 5, 6, 7 },{ 8, 9, 10, 11, 12, 13, 14 }};
-            dgDay.Rows.Clear();
-
-            dgDay.Rows.Add();
             dgDay["colDay1", 0].Value = ArrayTest[0,0];
             dgDay["colDay2", 0].Value = ArrayTest[0,1];
             dgDay["colDay3", 0].Value = ArrayTest[0,2];
@@ -45,15 +29,20 @@ namespace _0120
             dgDay["colDay5", 0].Value = ArrayTest[0,4];
             dgDay["colDay6", 0].Value = ArrayTest[0,5];
             dgDay["colDay7", 0].Value = ArrayTest[0,6];
+        }
 
-            dgDay["colDay1", 1].Value = ArrayTest[1,0];
-            dgDay["colDay2", 1].Value = ArrayTest[1,1];
-            dgDay["colDay3", 1].Value = ArrayTest[1,2];
-            dgDay["colDay4", 1].Value = ArrayTest[1,3];
-            dgDay["colDay5", 1].Value = ArrayTest[1,4];
-            dgDay["colDay6", 1].Value = ArrayTest[1,5];
-            dgDay["colDay7", 1].Value = ArrayTest[1,6];
-           
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();
+
+            dgDay.Rows.Add();
+                for (int i = 0; i < 2; ++i)
+                {
+                    for (int j = 1; j <= 7; ++j)
+                    {
+                        dgDay[$"colDay{j}", i].Value = ArrayTest[i, j-1];
+                    }
+                }
+            }
         }
     }
-}
