f9c7ec548c332375235c62955daef2d8a9d7682b
dragon <gildydtjd@naver.com>
2022-01-24 14:04:47 +0900

    Feat : Thread

--- 0124/0124/Program.cs
+++ 0124/0124/Program.cs
@@ -3,19 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace _0124
 {
     delegate int FuncDelegate(int a, int b);
 
-    static class Program
+    class Program
     {
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Program p = new Program();
+            p.Thread_go();
+        }
+
+        void Thread_go()
+        {
+            dragon_Thread d = new dragon_Thread();
+
+            Thread t1 = new Thread(new ThreadStart(d.Run));
+            t1.Start();
+
+            //Console.WriteLine("Interrupt");
+            //t1.Interrupt();
+
+            Console.WriteLine("IsAlive");
+            bool t1Bool = t1.IsAlive;
+            Console.WriteLine("지금 Thread의 실행 상태는 : "+ t1Bool );
+
+            Console.WriteLine("Suspend");
+            t1.Suspend();
+            Console.WriteLine("Suspend 잠자는중");
+            Thread.Sleep(4000);
+
+            Console.WriteLine("Resume");
+            t1.Resume();
+            Console.WriteLine("Resume 다시 시작");
+
+            //Console.WriteLine("Join");
+            //t1.Join();
+
+            Console.WriteLine("6초 뒤에 Abort");
+            Thread.Sleep(10000);
+            t1.Abort();
+            Console.WriteLine("종료");
         }
     }
 }
---
+++ 0124/0124/dragon_Thread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace _0124
+{
+    class dragon_Thread
+    {
+        public void Run()
+        {
+            for(int i=0; i<50; i++)
+            {
+
+                if (i != 0 &&i % 5 == 0)
+                {
+                    Thread.Sleep(3000);
+                    Console.WriteLine("나는 5의 배수이다.");
+                    Console.WriteLine(i);
+
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+                Console.WriteLine("Thread 종료");
+        }
+    }
+}
