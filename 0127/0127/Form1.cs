d8fad7f78977ff06c9190e118937988e1246db1b
dragon <gildydtjd@naver.com>
2022-01-27 11:01:20 +0900

    Feat : db connection

---
+++ 0127/0127/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0127
+{
+    public partial class Form1 : Form
+    {
+        private DataTable data_table = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                lock (DBcon.DBConn)
+                {
+                    if (!DBcon.IsDBConnected)
+                    {
+                        MessageBox.Show("db연결 안됨");
+                        return;
+                    }
+                    else
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter("select * from [dragon].[dbo].[user]", DBcon.DBConn);
+                        data_table = new DataTable();
+
+                        try
+                        {
+                            adapter.Fill(data_table);
+                            dataGridView1.DataSource = data_table;
+
+                            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("데이터 없음");
+                        }
+
+                        DBcon.Close();
+                    }
+                }
+            } catch
+            {
+                MessageBox.Show("gridbox 오류");
+            }
+        }
+    }
+}
